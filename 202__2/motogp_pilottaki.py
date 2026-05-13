3eb0b62a94e3e6373a3b600e99c6768f5d5f9069
BulyakiBence <bulyaki.bence.benedikt@diak.szbi-pg.hu>
2026-05-13 08:56:56 +0200

    3

--- 202__2/motogp_pilottaki.py
+++ 202__2/motogp_pilottaki.py
@@ -1,4 +1,4 @@
-class Pilotak:
+class Pilota:
     def __init__(self, nev, csapat, nemz, rajtsz, osszpont):
         self.nev = nev
         self.csapat = csapat
@@ -7,10 +7,45 @@ class Pilotak:
         self.osszpont =  int(osszpont)
 
 fajl = open("202__2/pilottak.txt" , "r", encoding="utf-8")
+pilotak = []
 for sor in fajl:
     adatok = sor.strip().split(";")
-    uj = Pilotak(
+    uj = Pilota(
         adatok[0],
-
-
+        adatok[1],
+        adatok[2],
+        adatok[3],
+        adatok[4]
     )
+    pilotak.append(uj)
+fajl.close()
+print("3.2 feladat")
+print(f"Az állományban {len(pilotak)} pilóta adatai szerepelnek")
+
+legjobb = pilotak[0]
+for p in pilotak:
+    if p.osszpont > legjobb.osszpont:
+        legjobb = p
+print(" 3.3 feladat.A legtöbb pontot sze3rző pilóta:")
+print(f"Név:{legjobb.nev}\n Csapat: {legjobb.csapat} \n Nemzetiség: {legjobb.nemz} \n Rajtszám {legjobb.rajtszam} \n Pontszám:{legjobb.osszpont}")
+
+
+print("3.4 feladat")
+csap = str(input("Mondj egy csapatot: "))
+van = False
+for p in pilotak:
+    if p.csapat == csap:
+        print(p.nev)
+        van = True
+if van == False:
+    print("Nincsen ilyen csapat.")
+
+#3.5 feladat
+ossz = 0
+darab = 0
+for p in pilotak:
+    if p.nemz == "spanyol":
+        ossz += p.osszpont
+        darab +=1
+atlag = ossz/darab
+print(f"A spanyol őpilóták átlagosan {atlag:.2f} pontot szereztek")    
