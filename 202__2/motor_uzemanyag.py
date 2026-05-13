fd32f88862349c0e338d09cde1d86bc45ca93078
BulyakiBence <bulyaki.bence.benedikt@diak.szbi-pg.hu>
2026-05-13 10:20:28 +0200

    Update motor_uzemanyag.py

--- 202__2/motor_uzemanyag.py
+++ 202__2/motor_uzemanyag.py
@@ -1,11 +1,11 @@
-korok = int(input("Adja meg a teljesitendo korok szamat: "))
-fogyasztas = float (input("Adja meg az átlag fogyasztást literben  egy körre:  "))
-tartaly = int(input("Adja meg a tartály méreteét literben: "))
+korok = int(input("Köröki száma: "))
+fogyasztas = float (input("Átlagos fogyasztás körönént (literben):  "))
+tartaly = int(input("tartály méret (liter): "))
 szukseges = korok*fogyasztas
 print(f"A versenyhez szükséges üzemanyag {szukseges} liter")
 if szukseges < tartaly:
-    print("a tartály elegendő a versenyhez")
+    print("A tartály elegendő a versenyhez!")
 else:
     szukseges > tartaly
     kell = szukseges - tartaly
-    print(f"Nem elegendő a tartály. Ennyi üzemanyag kell még {kell} liter")
+    print(f"A tartály NEM elegend. Ennyi üzemanyag kell még {kell} liter")
